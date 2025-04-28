3b9837edf905e763070b25896e3a6427b41f5050
yamaxbot <zakiryanov.ilyas.il@mail.ru>
2025-04-28 19:45:22 +0300

    Начальный коммит

--- app/keyboards.py
+++
@@ -1,7 +0,0 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-client_inline_keyboards = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text='Купить', callback_data='buy'), InlineKeyboardButton(text='Продать', callback_data='sell')],
-    [InlineKeyboardButton(text='Помощь', callback_data='help'), InlineKeyboardButton(text='О проекте', callback_data='project')]
-
-])
